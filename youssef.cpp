10ee1ea129b2d6cfd61e91df97b01a190a4917dd
Yousseff Muhammed EL Masry <35680381+yousefmasry4@users.noreply.github.com>
2019-04-21 04:01:09 +0200

    Add files via upload

---
+++ youssef.cpp
@@ -0,0 +1,155 @@
+#include <iostream> 
+#include <queue> 
+#include <string.h>
+#include <vector>
+#include <cstdlib>
+using namespace std; 
+struct primarytemp
+{
+  primarytemp() : process(), secondary() {}
+  primarytemp(double process, double secondary) 
+  : process(process), secondary(secondary) {}
+
+  double process;
+  double secondary; 
+};
+struct CompareHeight { 
+    bool operator()(primarytemp const& p1, primarytemp const& p2) 
+    { 
+        // return "true" if "p1" is ordered  
+        // before "p2", for example: 
+        return p1.secondary < p2.secondary; 
+    } 
+}; 
+char fi(int x){
+    if(x > 0)
+        return '+';    
+}
+void getdata(int *a,int *b){
+    char input[100];
+    gets(input);
+    for(int i=0;i<strlen(input);i++){
+        int tt,v,zz;
+    primarytemp p;
+        string data="";
+        int ff=0;
+        if(input[i] !='x' ){
+        for(v=i;v<strlen(input);v++){
+            if(input[v] != 'x' ){
+                data+=input[v];
+            }else{
+                break;
+            }
+                      //      cout<<tt;
+        }
+        tt=v;
+        i=tt;
+
+        *a=atoi(data.c_str());
+    }else
+    {
+        *a=1;
+    }
+    
+        if(input[i] == 'x'){
+        //    puts("yes");
+            string data2="";
+            for(zz=i+1;zz<strlen(input);zz++){
+         //       puts("dddddddddddddd");
+                if(input[zz] != 'x' && input[zz] != '+' && input[zz] != '-'){
+                    data2+=input[zz];
+                    ff=1;
+                }else{
+                    break;
+                }
+                tt=zz;
+            }
+            i=tt;
+            if(ff == 0){
+                *b=1;
+            }else
+            *b=atoi(data2.c_str());
+        }else{
+            p.secondary=0;
+        }
+        i=tt;
+    }
+}
+
+int main(){
+    char input[100];
+    gets(input);
+    priority_queue<char> op;
+    queue<primarytemp> q;
+    primarytemp p;
+
+    for(int i=0;i<strlen(input);i++){
+        int tt,v,zz;
+    primarytemp p;
+        string data="";
+        if(input[i] !='x' ){
+        for(v=i;v<strlen(input);v++){
+            if(input[v] != 'x' ){
+                data+=input[v];
+            }else{
+                break;
+            }
+                      //      cout<<tt;
+        }
+        tt=v;
+        i=tt;
+
+        p.process=atoi(data.c_str());
+    }else
+    {
+        p.process=1;
+    }
+    
+        if(input[i] == 'x'){
+            cout << "";
+        //    puts("yes");
+            string data2="";
+            int ff=0;
+            for(zz=i+1;zz<strlen(input);zz++){
+         //       puts("dddddddddddddd");
+                if(input[zz] != 'x' && input[zz] != '+' && input[zz] != '-'){
+                    ff=1;
+                    data2+=input[zz];
+                }else{
+                    break;
+                }
+                tt=zz;
+            }
+            i=tt;
+            if(ff ==0){
+                p.secondary=1;
+            }else
+            p.secondary=atoi(data2.c_str());
+        }else{
+            puts("");
+            p.secondary=0;
+        }
+        i=tt;
+        q.push(p);
+    }
+    int a=0,b=0;
+    getdata(&a,&b);
+    int flag=0;
+    while(!q.empty()){
+        primarytemp x=q.front();
+        q.pop();
+        if(flag == 0 && b>x.secondary){
+            cout<<fi(a)<<a<<"x^("<<b<<')';
+            flag=1;
+        }
+        if(b == x.secondary && flag==0){
+            flag=1;
+            x.process+=a;
+        }
+        cout<<fi(x.process)<<x.process<<"x^("<<x.secondary<<')';
+       // break;
+    }
+    if(flag == 0){
+        cout<<fi(a)<<a<<"x^("<<b<<')';
+    }
+}
